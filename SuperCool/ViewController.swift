024b9af85e0447f0ef1a338d4f690bc03e7efe01
Forest <Forest@Ricks-Mac-mini.local>
2016-03-25 16:48:18 -0400

    added files for Supercool

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var boomLabel: UIImageView!
 
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+    @IBOutlet weak var boringbutton: UIButton!
+    @IBOutlet weak var uncoolButton: UIButton!
+    @IBAction func coolButton(sender: AnyObject) {
+        boomLabel.hidden = false
+        backgroundImage.hidden = false
+        uncoolButton.hidden = true
+        boringbutton.hidden = false
+    }
+    @IBAction func makemeBoring(sender: AnyObject) {
+        boomLabel.hidden = true
+        backgroundImage.hidden = true
+        boringbutton.hidden = true
+        uncoolButton.hidden = false
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        boomLabel.hidden = true
+        backgroundImage.hidden = true
+        boringbutton.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
